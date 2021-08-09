4d38533de6027f3de1eb5dd45194bb79801fee33
hungba <lengochung.ptit@gmail.com>
2021-08-09 14:25:53 +0700

    change comment

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Ky Thuat Ha Tang Team\n');
+  res.send('Hello ktht team\n');
 });
 
 app.listen(PORT, HOST);
