51985b6687f1325bf129ea9cd8a9d0b5f86bd9b5
hungba <lengochung.ptit@gmail.com>
2021-08-10 09:00:30 +0700

    change jenkinsaaaaaa

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello ktht team\n');
+  res.send('Hello Everybody\n');
 });
 
 app.listen(PORT, HOST);
